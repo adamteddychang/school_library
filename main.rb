6fdfae2d2ccf696b65f58ca5a6105c69913c9218
adamteddychang <adamchang0725@gmail.com>
2022-01-31 14:46:59 +0100

    fixed linters

--- main.rb
+++ main.rb
@@ -1,30 +1,31 @@
 require_relative 'app'
 class Main
- def initialize 
-  @app = App.new
- end
-
- def start
-  puts 'Welcome to our School Library!'
-  loop do
-    menu
-    option = gets.chomp
-    break if option == '7'
-    @app.get_num(option)
+  def initialize
+    @app = App.new
   end
-  puts 'Thank you for using our Library!'
-end
 
-def menu
-  puts
-  puts 'Please choose an option by entering a number:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
-end
+  def start
+    puts 'Welcome to our School Library!'
+    loop do
+      menu
+      option = gets.chomp
+      break if option == '7'
+
+      @app.get_num(option)
+    end
+    puts 'Thank you for using our Library!'
+  end
+
+  def menu
+    puts
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
 end
 Main.new.start
